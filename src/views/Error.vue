1553c2456666418fbd74cf38de6f4dab6904892c
efrain <efraingpj@gmail.com>
2020-07-11 11:50:16 -0300

    initial commit

---
+++ src/views/Error.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="flex-col items-center">
+    <h1 class="text-gray-700 text-6xl uppercase">
+      Error
+    </h1>
+    <router-link class="mt-5 text-xl text-green-600 hover:underline">
+      Volver a la pagina anterior</router-link
+    >
+  </div>
+</template>
