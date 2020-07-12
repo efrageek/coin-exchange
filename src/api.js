824eda67c9a084833583e7087f534e279942bc68
efrain <efraingpj@gmail.com>
2020-07-12 11:15:58 -0300

    dynamic routes added

--- src/api.js
+++ src/api.js
@@ -6,13 +6,40 @@ async function getAssets() {
     const dataJSON = await dataFetched.json();
     const data = await dataJSON.data;
 
-    console.log(data);
     return data;
   } catch (e) {
     console.log(e);
   }
 }
 
+async function getAsset(coin) {
+  try {
+    const dataFetched = await fetch(`${URL}/assets/${coin}`);
+    const dataJSON = await dataFetched.json();
+    const data = await dataJSON.data;
+
+    return data;
+  } catch (e) {
+    console.log(e);
+  }
+}
+
+async function getAssetHistory(coin) {
+  const now = new Date();
+  const end = now.getTime();
+  now.setDate(now.getDate() - 1);
+  const start = now.getTime();
+
+  const dataFetched = await fetch(
+    `${URL}/assets/${coin}/history?interval=h1&start=${start}&end=${end}`
+  );
+  const dataJSON = await dataFetched.json();
+  const data = await dataJSON.data;
+  return data;
+}
+
 export default {
-  getAssets
+  getAssets,
+  getAsset,
+  getAssetHistory
 };
